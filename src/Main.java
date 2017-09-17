22938b36edb2558cca37cd2c523768dbfc2bcd48
imsamiulla <32022078+imsamiulla@users.noreply.github.com>
2017-09-17 16:32:12 +0530

    commit2

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,7 @@ public class Main {
 		//You may test that your code works find here
 		//Please check that your code works and has no 
 		//compilation problems before to submit
-		Student st1 = new Student(01,"samiulla shaik",new Date(1996,01,06),63.568);
-		Student st2 = new Student(02,"m saiswi",new Date(1996,01,06),70.568);
-		Student st3 = new Student(03,"eluri supriya",new Date(1996,01,06),60.568);
-		Student st4 = new Student(04,"wahadath jahan",new Date(1996,01,06),73.568);
+		
 		
 	}
 
